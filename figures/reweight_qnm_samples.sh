b1fd8b996c1f6ac8771edce3d4ff53ed4da896c2
Collin Capano <collin.capano@aei.mpg.de>
2022-09-29 04:14:43 +0000

    fix reweighted posterior

--- figures/reweight_qnm_samples.sh
+++ figures/reweight_qnm_samples.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/bash
 # Run the reweight script to reweight the Kerr results to one corresponding
 # to a prior that is uniform in mass ratio
-python reweight_qnm_samples.py --input-file \
-    ../posteriors/kerr/220_330/KERR-220_330-06MS.hdf \
-    --output-file ../posteriors/reweighted/REWEIGHTED_KERR-220_330-06MS.hdf
+python reweight_qnm_samples.py \
+    --input-file \
+        ../../reruns/more_livepoints/220_330/samples-06.hdf \
+    --output-file \
+        ../posteriors/reweighted/reweighted_samples-kerr-220_330-06ms.hdf 
+
+pycbc_inference_extract_samples --verbose --force \
+    --input-file \
+        ../posteriors/reweighted/reweighted_samples-kerr-220_330-06ms.hdf \
+    --output-file ../posteriors/reweighted/REWEIGHTED_KERR-220_330-06MS.hdf \
+    --skip-groups sampler_info
